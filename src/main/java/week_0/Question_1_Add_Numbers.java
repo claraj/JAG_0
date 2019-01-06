44ca973042f32247c4880da8e776832d8d9ec77d
clara <clara@minneapolis.edu>
2019-01-06 12:47:47 -0600

    Update Docker image to JDK 11

--- src/main/java/week_0/Question_1_Add_Numbers.java
+++ src/main/java/week_0/Question_1_Add_Numbers.java
@@ -31,7 +31,7 @@ public class Question_1_Add_Numbers {
         // It should add num1 and num2 and return the result.
         // For example, if num1 = 5 and num2 = 3, your code should calculate 5 + 3 = 8
         
-        return 0;
+        return num1 + num2;
 
     }
 
